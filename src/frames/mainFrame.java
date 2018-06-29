858dc75685e04881582aeba3465359634b39f28d
DimitriLeandro <dimitri.leandro@gmail.com>
2018-06-29 14:12:14 -0300

    Versão Beta finalizada
    Falta criar um instalador bonitinho...

--- src/frames/mainFrame.java
+++ src/frames/mainFrame.java
@@ -3,9 +3,11 @@ package frames;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,7 +17,8 @@ import javax.swing.JOptionPane;
 public class mainFrame extends javax.swing.JFrame {
 
     Scanner objSc;
-    String textoFinal = "";
+    String textoFinal;
+    JFileChooser objFC;
 
     public mainFrame() {
         initComponents();
@@ -27,11 +30,12 @@ public class mainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         btnOk = new javax.swing.JButton();
-        txtArquivo = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Open BCI File Handler");
+        setResizable(false);
 
-        btnOk.setText("Abrir");
+        btnOk.setText("Selecionar Arquivos");
         btnOk.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnOkActionPerformed(evt);
@@ -44,76 +48,84 @@ public class mainFrame extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(txtArquivo, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnOk, javax.swing.GroupLayout.PREFERRED_SIZE, 282, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnOk, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(txtArquivo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(btnOk)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(btnOk, javax.swing.GroupLayout.DEFAULT_SIZE, 53, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOkActionPerformed
-        String texto = txtArquivo.getText();
+        objFC = new JFileChooser();
+        objFC.setMultiSelectionEnabled(true);
+        //objFC.showOpenDialog(this); 
 
-        File objFile = new File(texto);
-
-        //quando o scanner for abrir o arquivo, ele pode n ão existir, portanto, tenta ai, se não rolar, me mostra ai no system.out qual foi o erro.
-        try {
-            objSc = new Scanner(objFile);
-            lerTxt();
-            criarNovoTxt();
-        } catch (Exception e) {
-            System.out.println(e);
-            JOptionPane.showMessageDialog(null, "Algo deu errado ao abrir o txt");
+        int returnVal = objFC.showSaveDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            try {
+                File objFile[] = objFC.getSelectedFiles();
+                for (int i = 0; i < objFile.length; i++) {
+                    System.out.println("EXECUTANDO CÓDIGO PARA O ARQUIVO " + objFile[i].getName());
+                    textoFinal = "";
+                    lerTxt(objFile[i]);
+                    criarNovoTxt(objFile[i]);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                JOptionPane.showMessageDialog(null, "Algo deu errado ao abrir os txts");
+            }
         }
     }//GEN-LAST:event_btnOkActionPerformed
 
-    public void criarNovoTxt() {
-        String nomeDoArquivo = "novo_" + txtArquivo.getText();
-        
+    public void criarNovoTxt(File objFile) {
+        //o .getParent pega a pasta em que o arquivo está e o .getName pega o nome do arquivo. Então a variável novoArquivo vai ficar tipo "/pasta/para/o/arquivo/novo_nomedoarquivo.txt"
+        String novoArquivo = objFile.getParent() + "/novo_" + objFile.getName();
+
         try {
-            PrintWriter escritorDeTxt = new PrintWriter(nomeDoArquivo, "UTF-8"); //criando um objeto da classe de escrever txt's
+            PrintWriter escritorDeTxt = new PrintWriter(novoArquivo, "UTF-8"); //criando um objeto da classe de escrever txt's
             escritorDeTxt.print(textoFinal); //adicionando linhas no txt
             escritorDeTxt.close(); //destruindo o objeto
-        } catch (Exception e) {
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
             System.out.println(e);
         }
     }
 
-    public void lerTxt() {
-        String linha;
-        boolean chegouL1 = false;
+    public void lerTxt(File objFile) {
+        try {
+            objSc = new Scanner(objFile);
+            String linha;
+            boolean chegouL1 = false;
 
-        while (objSc.hasNextLine()) {
-            linha = objSc.nextLine();
+            while (objSc.hasNextLine()) {
+                linha = objSc.nextLine();
 
-            if (chegouL1 == false) {
-                String primeiroCaracter = String.valueOf(linha.charAt(0));
+                if (chegouL1 == false) {
+                    String primeiroCaracter = String.valueOf(linha.charAt(0));
 
-                if (primeiroCaracter.equals("1")) {
-                    chegouL1 = true; //chegamos na primira linha!
-                    System.out.println(linha);
-                    textoFinal = textoFinal + linha;
-                }
-            } else {
-                //antes de exibir a linha, eu tenho que verificar se ela não é a última. Se for, eu não devo exibi-la
-                if (objSc.hasNextLine()) { //se tiver mais uma linha, significa que a linha atual não é a ultima, portanto, exiba-a
-                    System.out.println(linha);
-                    textoFinal = textoFinal + "\n" + linha;
+                    if (primeiroCaracter.equals("1")) {
+                        chegouL1 = true; //chegamos na primira linha!
+                        //System.out.println(linha);
+                        textoFinal = textoFinal + linha;
+                    }
+                } else {
+                    //antes de exibir a linha, eu tenho que verificar se ela não é a última. Se for, eu não devo exibi-la
+                    if (objSc.hasNextLine()) { //se tiver mais uma linha, significa que a linha atual não é a ultima, portanto, exiba-a
+                        //System.out.println(linha);
+                        textoFinal = textoFinal + "\n" + linha;
+                    }
                 }
             }
+            
+            objSc = null; //limpando a memória do objeto scanner
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
         }
     }
 
@@ -144,6 +156,5 @@ public class mainFrame extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnOk;
-    private javax.swing.JTextField txtArquivo;
     // End of variables declaration//GEN-END:variables
 }
